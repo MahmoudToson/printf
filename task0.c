cf04a87a821f3004f9d7b6ee6635bc9bc1b7b892
Your Name <Mahmoudtat99@gmail.com>
2023-10-17 10:58:58 -0700

    a

--- task0.c
+++ task0.c
@@ -1,41 +1,55 @@
 #include "main.h"
-int _printf(const char *format, ...){
-
+/**
+ *_printf: prints string and chars
+ *
+ *Description: prints anything
+ *
+ * @format: format string
+ *
+ * Return: string and chars
+*/
+int _printf(const char *format, ...)
+{
 int char_counter = 0;
 va_list args_list;
-va_start(args_list, format); 
-if(format == NULL)
-return(-1);
-while(*format){
-if(*format != '%'){
+va_start(args_list, format);
+if (format == NULL)
+return (-1);
+while (*format)
+{
+if (*format != '%')
+{
 write(1, format, 1);
 char_counter++;
 }
-else{
+else
+{
 format++;
-if(*format == 'c'){
+if (*format == 'c')
+{
 char c = va_arg(args_list, int);
 write(1, &c, 1);
 char_counter++;
 }
-if(*format == 's'){
+if (*format == 's')
+{
 int str_chain = 0;
-int i;
 char *str = va_arg(args_list, char*);
-while(str[str_chain] != '\0')
+while (str[str_chain] != '\0')
 str_chain++;
 write(1, str, str_chain);
 char_counter += str_chain;
 }
-if(*format == '%'){
+if (*format == '%')
+{
 write(1, format, 1);
 char_counter++;
 }
-if(*format == '\0')
+if (*format == '\0')
 break;
 }
 format++;
 }
 va_end(args_list);
-return char_counter;
+return (char_counter);
 }
